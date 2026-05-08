63be22cf465b9099dcc20be4b80873e15e16133a
kevin <kevin@gmail.com>
2026-05-08 11:48:29 -0500

    agregar

--- src/Scripts/sistema.interactivo.js
+++ src/Scripts/sistema.interactivo.js
@@ -5,5 +5,5 @@ if (age <= 18) {
     // Mostramos una alerta cuando la edad es igual a 0 
     alert('¡Sigue aprendiendo y disfrutando del código!')
 }else{
-    alert( 'su nombre es :' + name + ' ' + 'su edad es: ' +  age + '¡Prepárate para grandes oportunidades en el mundo de la programación!"')
+    alert( 'su nombre es ' + name + ' ' + 'su edad es ' +  age + '¡Prepárate para grandes oportunidades en el mundo de la programación!')
 }
