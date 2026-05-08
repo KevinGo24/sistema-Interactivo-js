d8102bafa760de4eb2b3e3d7d112772a051a6aca
kevin <kevin@gmail.com>
2026-05-08 12:38:27 -0500

    agregar

--- src/Scripts/sistema.interactivo.js
+++ src/Scripts/sistema.interactivo.js
@@ -1,9 +1,11 @@
-const name = String(prompt('digite su nombre: '))
-const age = Number(prompt('digite su edad: '))
+let name = String(prompt('digite su nombre: '))
+let age = Number(prompt('digite su edad: '))
 // Hacemos condicionbal para validar la edad 
-if (age <= 18) {
-    // Mostramos una alerta cuando la edad es igual a 0 
-    alert('¡Sigue aprendiendo y disfrutando del código!')
-}else{
-    alert( 'su nombre es ' + name + ' ' + 'su edad es ' +  age + '¡Prepárate para grandes oportunidades en el mundo de la programación!')
+// 2. Validamos
+if (age < 18){
+    // Esto se ejecuta solo si la edad es exactamente 0
+    alert('¡Sigue aprendiendo y disfrutando del código!');
+} else {
+    // Esto se ejecuta si es 18 o más
+    alert(`Bienvenido, ${name}!  preparate para algo importante en este mundo de la programacion`);
 }
